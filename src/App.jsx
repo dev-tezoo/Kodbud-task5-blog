be5de0b78569aaa911027a8f413e91240715d362
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-15 20:44:02 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-white'>App</div>
+  <>
+   <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+    
+   </div>
+  </>
   )
 }
 
